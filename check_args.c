b9a680c24cd4f2394a71568c72c0985e7ac1b967
Nuno Valadão <111917125+nunovaladao@users.noreply.github.com>
2023-04-19 22:46:24 +0100

    Add files via upload

--- check_args.c
+++ check_args.c
@@ -3,75 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   check_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 20:24:27 by nuno              #+#    #+#             */
-/*   Updated: 2023/04/19 12:45:02 by nuno             ###   ########.fr       */
+/*   Updated: 2023/04/15 18:41:16 by nsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int input_window(char **av, char x)
+void	display_mensg(int options)
 {
-    int i;
-    int size_win;
-
-    i = 1;
-    size_win = 0;
-    while (av[++i])
-    {
-        if ((x == 'w' && (i == 2)) || (x == 'h' && (i == 3)))
-        {
-            size_win = ft_atoi(av[i]);
-            break;
-        }
-    }
-    if (!size_win)
-        return (0);
-    if ((x = 'h') && (size_win > 0 && size_win <= 1080))
-        return (size_win);
-    else if ((x = 'w') && (size_win > 0 && size_win <= 1980))
-        return (size_win);
-    else
-    {
-        ft_printf("Put the right window size!\n\n");
-        ft_printf("width: 0 - 1980 and height: 0 - 1080\n");
-        exit(EXIT_FAILURE);
-    }
-    return (0);
+	if (options == 0)
+	{
+		ft_printf("Run:\n\n");
+		ft_printf("  1) ./fractol <fractal>\n");
+		ft_printf("  2) ./fractol <fractal> size_width size_height\n\n");
+		ft_printf("Fractal options:\n\n");
+		ft_printf("  - Julia\n  - Mandelbrot\n");
+		exit(EXIT_SUCCESS);
+	}
+	if (options == 1)
+	{
+		ft_printf("Options:\n");
+		ft_printf("\t- Click on ESC or on the cross for leave!\n");
+		ft_printf("\t- You also can do ZOOM_IN and ZOOM_OUT!\n");
+	}
+	if (options == 2)
+	{
+		ft_printf("Put the right window size!\nPut only numbers!\n\n");
+		ft_printf("width: 0 - 1980 and height: 0 - 1080\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-/* int window_size(char **av, t_vars *vars)
+int	input_window(char **av, char x)
 {
-    vars->width_win = input_window(av, 'w');
-    if (!vars->width_win)
-        vars->width_win = I_WIDTH;
-    vars->height_win = input_window(av, 'h');
-    if (!vars->height_win)
-        vars->height_win = I_HEIGHT;
-    return (0);
-} */
+	int	i;
+	int	size_win;
 
-void display_mensg(int options)
-{
-    if (options == 0)
-    {
-        ft_printf("Run:\n\n");
-        ft_printf("  1) ./fractol <fractal>\n");
-        ft_printf("  2) ./fractol <fractal> size_height size_width\n\n");
-        ft_printf("Fractal options:\n\n");
-        ft_printf("  - Julia\n  - Mandelbrot\n");
-    }
-    if (options == 1)
-    {
-        ft_printf("Options:\n");
-        ft_printf("\t- Click on ESC or on the cross for leave!\n");
-        ft_printf("\t- You also can do ZOOM_IN and ZOOM_OUT!\n");
-    }
+	i = 1;
+	size_win = 0;
+	while (av[++i])
+	{
+		if ((x == 'w' && (i == 2)) || (x == 'h' && (i == 3)))
+		{
+			size_win = ft_atoi(av[i]);
+			break ;
+		}
+	}
+	if (!size_win)
+		return (0);
+	if (x == 'w' && (size_win > 0 && size_win <= 1980))
+		return (size_win);
+	if (x == 'h' && (size_win > 0 && size_win <= 1080))
+		return (size_win);
+	display_mensg(2);
+	return (0);
 }
 
-int	check_args(int ac, char **av/* , t_vars *vars */)
+int	window_size(char **av, t_vars *vars)
+{
+	vars->width_win = input_window(av, 'w');
+	if (!vars->width_win)
+		vars->width_win = I_WIDTH;
+	vars->height_win = input_window(av, 'h');
+	if (!vars->height_win)
+		vars->height_win = I_HEIGHT;
+	return (0);
+}
+
+int	check_args(int ac, char **av, t_vars *vars)
 {
 	if (ac == 1 || ac == 3 || ac > 4)
 	{
@@ -83,10 +85,11 @@ int	check_args(int ac, char **av/* , t_vars *vars */)
 		if (ft_strcmp(av[1], "Julia") == 0 || ft_strcmp(av[1], \
 		"Mandelbrot") == 0)
 		{
-			/* if (ac == 4 && is_number(av[2]) == 0 && is_number(av[3]) == 0)
+			window_size(av, vars);
+			if (ac == 4 && is_number(av[2]) == 0 && is_number(av[3]) == 0)
 				window_size(av, vars);
 			else if (ac == 4)
-				display_mensg(2); */
+				display_mensg(2);
 			display_mensg(1);
 		}
 		else
